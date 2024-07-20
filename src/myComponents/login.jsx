53f953d5789888bbbd5bd9a6cd234f9d9efac1f5
techsysprogram <techsysprogram@gmail.com>
2024-07-20 15:05:14 +0200

    segundo commit

--- src/myComponents/login.jsx
+++ src/myComponents/login.jsx
@@ -2,15 +2,24 @@ import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 
 export function InputWithButton() {
-  return (
-<div className="flex items-center justify-center min-h-screen">
-  <div className="flex flex-col items-center w-full max-w-sm space-y-2">
-    <label className="text-lg font-semibold">Se connecter</label>
-    <Input type="email" placeholder="Email" className="w-full" />
-    <Input type="password" placeholder="Password" className="w-full" />
-    <Button type="submit" className="w-full">Subscribe</Button>
-  </div>
-</div>
 
+  function handleSubmit(e) {
+    e.preventDefault()
+    const fields = new FormData(e.target)
+    const fieldEmail = fields.get('email')
+    const fieldPassword = fields.get('password')
+    console.log(fieldEmail)
+    console.log(fieldPassword)
+  }
+
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+        <form className="form flex flex-col items-center w-full max-w-sm space-y-2" onSubmit={handleSubmit}>
+          <label className="text-lg font-semibold">Se connecter</label>
+          <Input name='email' type="email" placeholder="Email" className="w-full" />
+          <Input name='password' type="password" placeholder="Password" className="w-full" />
+          <Button type="submit" className="w-full">Subscribe</Button>
+        </form>
+    </div>
   )
 }
