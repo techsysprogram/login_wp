3f5f7d6c75d1b6cbe321d713cbbfefea92d025ee
techsysprogram <techsysprogram@gmail.com>
2024-07-27 12:01:48 +0200

    lpppolkhh

--- mon-code/includes/api-key.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (!defined('ABSPATH')) {
-    exit; // Exit if accessed directly
-}
-
-define('MY_API_KEY', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL3d3dy5yZWFjdC50ZWNoc3lzcHJvZ3JhbS5jb20iLCJpYXQiOjE3MjIwNjgyMTksIm5iZiI6MTcyMjA2ODIxOSwiZXhwIjoxNzIyNjczMDE5LCJkYXRhIjp7InVzZXIiOnsiaWQiOiIyIn19fQ.ooQ6CoyTpbAXbs2vQARHjk26lptQ1O7sPLL9Sr36XDg');
